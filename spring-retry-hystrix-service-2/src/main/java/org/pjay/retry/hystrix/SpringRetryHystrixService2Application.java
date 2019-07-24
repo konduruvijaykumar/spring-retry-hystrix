6facf97fdc8719dcd1df9af092da1a58e6e54db7
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-24 11:16:10 +0530

    spring-retry-hystrix-service-2

---
+++ spring-retry-hystrix-service-2/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService2Application.java
@@ -0,0 +1,13 @@
+package org.pjay.retry.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRetryHystrixService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRetryHystrixService2Application.class, args);
+	}
+
+}
