503fc686ccac645f9abad5ca00f16527867aeb61
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-24 17:32:44 +0530

    service1 hystrix circuit breaker test

--- spring-retry-hystrix-service-1/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService1Application.java
+++ spring-retry-hystrix-service-1/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService1Application.java
@@ -2,8 +2,10 @@ package org.pjay.retry.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class SpringRetryHystrixService1Application {
 
 	public static void main(String[] args) {
--- spring-retry-hystrix-service-1/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService1Controller.java
+++ spring-retry-hystrix-service-1/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService1Controller.java
@@ -10,11 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 /**
  * @author vijayk
@@ -57,6 +61,29 @@ public class SpringRetryHystrixService1Controller {
 		return customRestTemplate.exchange(ApplicationConstants.URL_SERVICE2_TIMEOUT, HttpMethod.GET, null,
 				new ParameterizedTypeReference<Result>() {
 				}, params);
+	}
+
+	@GetMapping("test-hystrix-cb/{timeOutMilliSec}")
+	@HystrixCommand(fallbackMethod = "testHystrixCBFallback", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "10000") })
+	public ResponseEntity<Result> testHystrixCircuitBreaker(@PathVariable long timeOutMilliSec) {
+		Map<String, String> params = new HashMap<String, String>();
+		if (timeOutMilliSec < 0) {
+			throw new RuntimeException("Time out value should be positive number");
+		}
+		params.put("timeOutMilliSec", (new Long(timeOutMilliSec)).toString());
+		return customRestTemplate.exchange(ApplicationConstants.URL_SERVICE2_TIMEOUT, HttpMethod.GET, null,
+				new ParameterizedTypeReference<Result>() {
+				}, params);
+	}
+
+	@SuppressWarnings("unused")
+	private ResponseEntity<Result> testHystrixCBFallback(long timeOutMilliSec) {
+		Result result = new Result();
+		result.setMessage(
+				"Response returned from testHystrixCBFallback method as we did not receive response from external service by configured time out "
+						+ 10000 + " milli seconds, where as passed time out is " + timeOutMilliSec + " milli seconds");
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
 }
