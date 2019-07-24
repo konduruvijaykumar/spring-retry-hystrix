7806e5d587da2c3cfd4730c2579a522e669912be
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-24 10:55:24 +0530

    spring-retry-hystrix

---
+++ spring-retry-hystrix-service-1/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService1Application.java
@@ -0,0 +1,13 @@
+package org.pjay.retry.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRetryHystrixService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRetryHystrixService1Application.class, args);
+	}
+
+}
---
+++ spring-retry-hystrix-service-1/src/test/java/org/pjay/retry/hystrix/SpringRetryHystrixService1ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.pjay.retry.hystrix;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRetryHystrixService1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
