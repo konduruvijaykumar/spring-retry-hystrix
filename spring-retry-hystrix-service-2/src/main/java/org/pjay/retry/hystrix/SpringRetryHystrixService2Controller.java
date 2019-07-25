aa99d9660e6e7bbfaca1cfb30513282d4375c965
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-25 15:03:28 +0530

    service2 logger changes

--- spring-retry-hystrix-service-2/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService2Controller.java
+++ spring-retry-hystrix-service-2/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService2Controller.java
@@ -5,6 +5,8 @@ package org.pjay.retry.hystrix;
 
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringRetryHystrixService2Controller {
 
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 	Random random = new Random();
 
 	@GetMapping(value = { "/", "/home", "/hello" })
@@ -27,6 +31,7 @@ public class SpringRetryHystrixService2Controller {
 
 	@GetMapping("service2-timeout/{timeOutMilliSec}")
 	public ResponseEntity<Result> service2Timeout(@PathVariable long timeOutMilliSec) {
+		log.info(" ## Start service2Timeout(@PathVariable long timeOutMilliSec) ## ");
 		try {
 			Thread.sleep(timeOutMilliSec);
 		} catch (InterruptedException e) {
@@ -35,18 +40,22 @@ public class SpringRetryHystrixService2Controller {
 		Result result = new Result();
 		result.setMessage(
 				"Response from service2-timeout API with time out sent as " + timeOutMilliSec + " milli seconds");
+		log.info(" ## End service2Timeout(@PathVariable long timeOutMilliSec) ## ");
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
 	@GetMapping("service2-success")
 	public ResponseEntity<Result> service2Success() {
+		log.info(" ## Start service2Success() ## ");
 		Result result = new Result();
 		result.setMessage("Response from service2-success API received");
+		log.info(" ## End service2Success() ## ");
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
 	@GetMapping("service2-exception")
 	public ResponseEntity<Result> service2Exception() {
+		log.info(" ## Start service2Exception() ## ");
 		@SuppressWarnings("unused")
 		Result result = new Result();
 		// oops some runtime exception occurred during the method execution
@@ -57,6 +66,7 @@ public class SpringRetryHystrixService2Controller {
 
 	@GetMapping("service2-random-exception")
 	public ResponseEntity<Result> service2RandomException() {
+		log.info(" ## Start service2RandomException() ## ");
 		Result result = new Result();
 		if (random.nextBoolean()) {
 			// oops some runtime exception occurred during the method execution
@@ -64,6 +74,7 @@ public class SpringRetryHystrixService2Controller {
 					"OOPS some runtime exception occurred during the service2-random-exception API execution");
 		}
 		result.setMessage("Response from service2 random exception API received");
+		log.info(" ## End service2RandomException() ## ");
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
