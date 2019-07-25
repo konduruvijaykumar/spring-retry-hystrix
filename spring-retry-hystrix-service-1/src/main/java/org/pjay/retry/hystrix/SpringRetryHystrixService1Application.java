603b13ba95b6c81b0b566e33cd73d6964d49aeba
Vijay Konduru <konduruvijaykumar@gmail.com>
2019-07-25 15:06:43 +0530

    service1 retry related API's work in progress

--- spring-retry-hystrix-service-1/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService1Application.java
+++ spring-retry-hystrix-service-1/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService1Application.java
@@ -5,11 +5,13 @@ import javax.annotation.PreDestroy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.retry.annotation.EnableRetry;
 
 import rx.schedulers.Schedulers;
 
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableRetry
 public class SpringRetryHystrixService1Application {
 
 	public static void main(String[] args) {
@@ -17,7 +19,7 @@ public class SpringRetryHystrixService1Application {
 	}
 
 	// https://github.com/Netflix/Hystrix/issues/1766
-	// This was done for below issue shown up to Hystrix Circuit breaker code, during server shutdown
+	// This was done for below issue shown up due to Hystrix Circuit breaker code, during server shutdown
 	/*
 	 * 2019-07-24 18:17:24.855 WARN 20884 --- [n(14)-127.0.0.1]
 	 * o.a.c.loader.WebappClassLoaderBase : The web application [ROOT] appears to
--- spring-retry-hystrix-service-1/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService1Controller.java
+++ spring-retry-hystrix-service-1/src/main/java/org/pjay/retry/hystrix/SpringRetryHystrixService1Controller.java
@@ -8,6 +8,8 @@ import static org.pjay.retry.hystrix.ApplicationConstants.URL_SERVICE2_TIMEOUT;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,9 +17,14 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -29,19 +36,112 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @RestController
 public class SpringRetryHystrixService1Controller {
 
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@Autowired
 	@Qualifier("customRestTemplate")
 	private RestTemplate customRestTemplate;
-	
+
 	@Value("${hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds:10000}")
 	private String hystrixCBTimeout;
 
 	@GetMapping(value = { "/", "/home", "/hello" })
 	public String hello() {
 		return "Hi there from Spring Retry Hystrix Service 1";
+	}
+
+	// https://stackoverflow.com/questions/45943715/how-to-configure-delay-time-in-spring-retry-spring-boot
+	// https://www.baeldung.com/spring-retry
+	/*
+	 * @Backoff attributes: delay - is the delay between retry attempts when an
+	 * exception occurs (default value is 1000).
+	 * 
+	 * @Backoff attributes: maxDelay - is the maximum delay or wait between retries.
+	 * 
+	 * @Backoff attributes: multiplier - a multiplier is used to use to calculate
+	 * the next backoff delay (default 0 = ignored) - See example below. If delay =
+	 * 1000, maxAttempts = 3 and multiplier = 2. Then 1st attempt delay = 1000, 2nd
+	 * = (1000 * 2) is 2000, 3rd = (2000 * 2) is 4000
+	 */
+	@GetMapping("retry-service-not-running")
+	@Retryable(value = { ResourceAccessException.class, RestClientResponseException.class,
+			Exception.class }, maxAttempts = 2, backoff = @Backoff(delay = 2000))
+	public ResponseEntity<Result> retryWithNoServiceRunning() {
+		return null;
+	}
+
+	@GetMapping("retry-no-service-api")
+	@Retryable(value = { ResourceAccessException.class, RestClientResponseException.class,
+			Exception.class }, maxAttempts = 2, backoff = @Backoff(delay = 2000))
+	public ResponseEntity<Result> retryWithNoServiceAPI() {
+		return null;
+	}
+
+	@GetMapping("retry-timeout-service/{timeOutMilliSec}")
+	@Retryable(value = { ResourceAccessException.class, RestClientResponseException.class,
+			Exception.class }, maxAttempts = 2, backoff = @Backoff(delay = 2000))
+	public ResponseEntity<Result> retryWithTiemoutService(@PathVariable long timeOutMilliSec) {
+		return null;
+	}
+
+	@GetMapping("retry-unknown-host")
+	@Retryable(value = { ResourceAccessException.class, RestClientResponseException.class,
+			Exception.class }, maxAttempts = 2, backoff = @Backoff(delay = 2000))
+	public ResponseEntity<Result> retryWithUnknownHost() {
+		return null;
+	}
+
+	@GetMapping("retry-test-resttemplate/{timeOutMilliSec}")
+	@Retryable(value = { ResourceAccessException.class, RestClientResponseException.class,
+			Exception.class }, maxAttempts = 2, backoff = @Backoff(delay = 2000))
+	public ResponseEntity<Result> retryWithRestTemplate(@PathVariable long timeOutMilliSec) {
+		Map<String, String> params = new HashMap<String, String>();
+		if (timeOutMilliSec < 0) {
+			throw new RuntimeException("Time out value should be positive number");
+		}
+		params.put("timeOutMilliSec", (new Long(timeOutMilliSec)).toString());
+		return restTemplate.exchange(URL_SERVICE2_TIMEOUT, HttpMethod.GET, null,
+				new ParameterizedTypeReference<Result>() {
+				}, params);
+	}
+
+	@Recover
+	private ResponseEntity<Result> recoverFromResourceAccessException(ResourceAccessException resourceAccessException) {
+		log.info(" ## Start recoverFromResourceAccessException(ResourceAccessException resourceAccessException) ## ");
+		Result result = new Result();
+		resourceAccessException.printStackTrace();
+		result.setMessage(resourceAccessException.getMessage());
+		result.setStatus("I/O error");
+		log.info(" ## End recoverFromResourceAccessException(ResourceAccessException resourceAccessException) ## ");
+		return new ResponseEntity<>(result, HttpStatus.SERVICE_UNAVAILABLE);
+	}
+
+	@Recover
+	private ResponseEntity<Result> recoverFromRestClientResponseException(
+			RestClientResponseException restClientResponseException) {
+		log.info(
+				" ## Start recoverFromRestClientResponseException(RestClientResponseException restClientResponseException) ## ");
+		Result result = new Result();
+		restClientResponseException.printStackTrace();
+		result.setMessage(restClientResponseException.getMessage());
+		result.setStatus(restClientResponseException.getStatusText());
+		log.info(
+				" ## End recoverFromRestClientResponseException(RestClientResponseException restClientResponseException) ## ");
+		return new ResponseEntity<>(result, HttpStatus.SERVICE_UNAVAILABLE);
+	}
+
+	@Recover
+	private ResponseEntity<Result> recoverFromException(Exception exception) {
+		log.info(" ## Start recoverFromException(Exception exception) ## ");
+		Result result = new Result();
+		exception.printStackTrace();
+		result.setMessage(exception.getMessage());
+		result.setStatus("Unknown");
+		log.info(" ## End recoverFromException(Exception exception) ## ");
+		return new ResponseEntity<>(result, HttpStatus.SERVICE_UNAVAILABLE);
 	}
 
 	@GetMapping("test-resttemplate/{timeOutMilliSec}")
@@ -79,8 +179,12 @@ public class SpringRetryHystrixService1Controller {
 	@HystrixCommand(fallbackMethod = "testHystrixCBFallback", commandProperties = {
 			@HystrixProperty(name = HYSTRIX_EXECUTION_TIMEOUT_IN_MILLISECONDS, value = HYSTRIX_TIMEOUT_VALUE) })
 	*/
+	/*
+	@HystrixCommand(fallbackMethod = "testHystrixCBFallback", commandKey = "testHystrixCB")
+	 */
 	// @formatter:on
-	// @HystrixCommand(fallbackMethod = "testHystrixCBFallback", commandKey = "testHystrixCB")
+	// Exceptions are transferred back to fallback method, without waiting for
+	// timeout, some exceptions can be ignored using attribute ignoreExceptions
 	@HystrixCommand(fallbackMethod = "testHystrixCBFallback")
 	public ResponseEntity<Result> testHystrixCircuitBreaker(@PathVariable long timeOutMilliSec) {
 		Map<String, String> params = new HashMap<String, String>();
@@ -98,8 +202,8 @@ public class SpringRetryHystrixService1Controller {
 		Result result = new Result();
 		result.setMessage(
 				"Response returned from testHystrixCBFallback method as we did not receive response from external service by configured time out "
-						+ /*HYSTRIX_TIMEOUT_VALUE*/ hystrixCBTimeout + " milli seconds, where as passed time out is " + timeOutMilliSec
-						+ " milli seconds");
+						+ /* HYSTRIX_TIMEOUT_VALUE */ hystrixCBTimeout + " milli seconds, where as passed time out is "
+						+ timeOutMilliSec + " milli seconds");
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
